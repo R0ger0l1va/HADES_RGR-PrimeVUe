76b362998fb71521edc78c0e73866c08a47e2bed
RogerOliva <rogeroliva44@gmail.com>
2024-12-02 12:19:35 +0100

    e
    as

--- HADES/src/App.vue
+++ HADES/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import {ref} from 'vue'
+const text = ref("")
 </script>
 
 <template>
-  
+  <div>
+    <InputText v-model="text"/> 
+    <Button label="Save"/>
+    
+  </div>
 </template>
 
 
--- HADES/src/main.js
+++ HADES/src/main.js
@@ -1,12 +1,9 @@
 import './assets/main.css'
-import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import Primevue from "primevue/config"
 import Aura from "@primevue/themes/aura"
 import App from './App.vue'
 import router from './router'
-import theme from 'tailwindcss-primeui/src/theme'
-import preset from 'tailwindcss-primeui/src/utils/preset'
 
 const app = createApp(App)
 
