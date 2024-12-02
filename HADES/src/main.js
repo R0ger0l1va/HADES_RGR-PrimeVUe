a32c96690c4da12d20bf4a7776bfe21d83edbf73
Roger Oliva <145022854+R0ger0l1va@users.noreply.github.com>
2024-12-02 05:42:42 -0500

    n

--- HADES/src/main.js
+++ HADES/src/main.js
@@ -1,10 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import Primevue from "primevue/config"
+import Aura from "@primevue/themes/aura"
 import App from './App.vue'
 import router from './router'
+import theme from 'tailwindcss-primeui/src/theme'
+import preset from 'tailwindcss-primeui/src/utils/preset'
 
 const app = createApp(App)
+app.use(Primevue,{
+    theme:{
+        preset:Aura,
+    }
+})
 
 app.use(router)
 
